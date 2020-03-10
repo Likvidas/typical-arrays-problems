e07d7b3e1c24bb8e913f9b1dd6d20ded73e4b900
Likvidas <bobento@yandex.ru>
2020-03-10 13:08:55 +0300

    init: start Typical Arrays Problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
+exports.min = function min(array) {
+    if (arguments.length === 0 || array.length === 0) return 0;
 
-exports.min = function min (array) {
-  return 0;
+    let minimum = Math.min(...array);
+
+    return minimum;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (arguments.length === 0 || array.length === 0) return 0;
+
+    let maximum = Math.max(...array);
+
+    return maximum;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (arguments.length === 0 || array.length === 0) return 0;
+
+    let averege = array.reduce((accum, current) => accum + current) / array.length;
+
+    return averege;
 }
